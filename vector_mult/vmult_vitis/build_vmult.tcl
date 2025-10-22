b7517b2dd0028ac2a19ef7d2c4f84ff0705aa0f4
sdrangan <sdrangan@gmail.com>
2025-10-22 15:33:50 -0400

    Corrected name for tb file

--- vector_mult/vmult_vitis/build_vmult.tcl
+++ vector_mult/vmult_vitis/build_vmult.tcl
@@ -2,7 +2,7 @@ open_project vmult_hls -reset
 set_top vec_mult
 
 add_files -cflags "-Iinclude" src/vmult.cpp
-add_files -tb testbench/vmult_tb.cpp
+add_files -tb testbench/tb_vmult.cpp
 
 open_solution "solution1" -reset
 set_part {xczu48dr-ffvg1517-2-e}
